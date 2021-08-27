1f0befb0ac1692d8a0954c11994e7f8831346fc0
murata <murata@kmc.gr.jp>
2021-08-28 07:39:18 +0900

    refactor

--- isutory.py
+++ isutory.py
@@ -4,12 +4,61 @@ import re
 from networkx import DiGraph
 from networkx.algorithms.isomorphism import DiGraphMatcher
 from networkx.drawing.nx_pydot import to_pydot
+from pprint import pprint
 
+# global keys
 URI = 'uri'
 METHOD = 'method'
 TIME = 'time'
-USER = 'ua'
+UA = "ua"
+VHOST = "vhost"
+APPTIME = "apptime"
+REQTIME = "reqtime"
+STATUS = "status"
+SIZE = "size"
+KEYS = [URI, METHOD, TIME, UA, VHOST, APPTIME, REQTIME, STATUS, SIZE]
+IDENTIFIER = 'ua'
 
+###  PARSE DATA
+def load_data(path):
+    data = []
+    with open(path) as f:
+        for line in f:
+            data.append(
+                {
+                    k: v for k, v in map(
+                        lambda x: x.split(':', 1),
+                        line.strip().split('\t')
+                    )
+                    if k in KEYS
+                }
+            )
+    return data
+
+def is_ignored(uri, ignore):
+    for i in ignore:
+        if i and i.match(uri):
+            return True
+    return False
+
+def filter_ignored_uri(data, ignore):
+    ignore = [re.compile(i) for i in ignore if i]
+    return list(filter(lambda d: not is_ignored(d[URI], ignore), data))
+
+def unify_uri(uri, aggregates):
+    for a in aggregates:
+        if a.match(uri):
+            return a.pattern
+    return uri
+
+def aggregate(data, aggregates):
+    aggregates = [re.compile(a) for a in aggregates if a]
+    for i in range(len(data)):
+        data[i][URI] = unify_uri(data[i][URI], aggregates)
+    return data
+
+
+### CREATE STORY GRAPH
 class IDGenerator:
     def __init__(self):
         self._dict = dict()
@@ -23,52 +72,15 @@ class IDGenerator:
 
 _id = IDGenerator()
 
-def load_data(path, necessary_keys):
-    data = []
-    with open(path) as f:
-        for line in f:
-            data.append(
-                {
-                    k: v for k, v in map(
-                        lambda x: x.split(':', 1),
-                        line.strip().split('\t')
-                    )
-                    if k in necessary_keys
-                }
-            )
-    return data
-
-def is_ignored(uri, ignore):
-    for i in ignore:
-        if i.match(uri):
-            return True
-    return False
-
-def filter_ignored_uri(data, ignore):
-    ignore = [re.compile(i) for i in ignore]
-    return list(filter(lambda d: not is_ignored(d[URI], ignore), data))
-
-def unify_uri(uri, aggregates):
-    for a in aggregates:
-        if a.match(uri):
-            return a.pattern
-    return uri
-
-def aggregate(data, aggregates):
-    aggregates = [re.compile(a) for a in aggregates]
-    for i in range(len(data)):
-        data[i][URI] = unify_uri(data[i][URI], aggregates)
-    return data
-
-def req(t):
-    return "%s %s" % (t[0], t[1])
-
 def node(r, cnt=0):
     """node returns node's name.
-    
+
     pydot doesn't escape names of nodes for DOT, so it's better to convert URI to integers.
     """
     return _id["%d:%s" % (cnt, r)]
+
+def req(t):
+    return "%s %s" % (t[0], t[1])
 
 def create_story(timeline, story_id):
     t = timeline
@@ -89,11 +101,11 @@ def create_story(timeline, story_id):
 def create_stories(data):
     timelines = dict()
     for d in data:
-        nodes, timeline = timelines.get(d[USER], (set(), list()))
+        nodes, timeline = timelines.get(d[IDENTIFIER], (set(), list()))
         t = (d[METHOD], d[URI], d[TIME])
         nodes.add(req(t))
         timeline.append(t)
-        timelines[d[USER]] = (nodes, timeline)
+        timelines[d[IDENTIFIER]] = (nodes, timeline)
     for k in timelines.keys():
         timelines[k][1].sort(key=lambda t: t[2])
     stories = DiGraph()
@@ -112,11 +124,11 @@ def create_stories(data):
 def create_unified_graph(data):
     timelines = dict()
     for d in data:
-        timeline = timelines.get(d[USER], list())
+        timeline = timelines.get(d[IDENTIFIER], list())
         t = (d[METHOD], d[URI], d[TIME])
         timeline.append(t)
-        timelines[d[USER]] = timeline
-    from_ = dict()
+        timelines[d[IDENTIFIER]] = timeline
+    src = dict()
     for timeline in timelines.values():
         timeline.sort(key=lambda t: t[2])
         if len(timeline) < 2:
@@ -124,22 +136,22 @@ def create_unified_graph(data):
         for t1, t2 in zip(timeline, timeline[1:]):
             r1 = req(t1)
             r2 = req(t2)
-            total, to_ = from_.get(r1, (0, Counter()))
-            to_[r2] += 1
-            from_[r1] = (total + 1, to_)
+            total, dst = src.get(r1, (0, Counter()))
+            dst[r2] += 1
+            src[r1] = (total + 1, dst)
     stories = DiGraph()
-    for f, (total, to_) in from_.items():
+    for f, (total, dst) in src.items():
         nf = node(f)
         stories.add_node(nf, label=f)
-        for t, cnt in to_.items():
+        for t, cnt in dst.items():
             nt = node(t)
             stories.add_node(nt, label=t)
             stories.add_edge(nf, nt, color="#000000%02X" % (16 + 240 * cnt // total))
     return stories
 
-def show_graph(stories, out):
+def write_graph(stories, out):
     pd = to_pydot(stories)
-    if out.endswith(".svg"):
+    if out.endswith(".svg") or out.endswith(".html"):
         pd.write_svg(out)
     elif out.endswith(".dot"):
         pd.write_dot(out)
@@ -149,11 +161,11 @@ def show_graph(stories, out):
         raise ValueError("unexpected extension: %s" % out)
 
 def main(args):
-    global USER
-    USER = args.user
-    data = load_data(args.ltsv, [URI, METHOD, TIME, USER])
+    data = load_data(args.ltsv)
     if len(args.ignore) == 1:
         args.ignore = args.ignore[0].split(',')
+    else:
+        args.ignore = [""]
     data = filter_ignored_uri(data, args.ignore)
     if len(args.aggregates) == 1:
         args.aggregates = args.aggregates[0].split(',')
@@ -162,15 +174,17 @@ def main(args):
         stories = create_unified_graph(data)
     else:
         stories = create_stories(data)
-    show_graph(stories, args.out)
+    write_graph(stories, args.out)
 
 if __name__ == '__main__':
     parser = ArgumentParser()
     parser.add_argument('ltsv', help='nginx log formatted in LTSV')
-    parser.add_argument('--aggregates', nargs='*', help='URL aggregation')
-    parser.add_argument('--ignore', nargs='*', help='URL to ignore')
+    parser.add_argument('--aggregates', default="", nargs='*', help='URL aggregation')
+    parser.add_argument('--ignore', default="", nargs='*', help='URL to ignore')
     parser.add_argument('--unified', action='store_true', help='show unified graph which highlights where to go')
-    parser.add_argument('--user', '-u', default=USER, help='label of user identifier')
-    parser.add_argument('--out', '-o', default='stories.svg', help='name of output svg file')
+    parser.add_argument('--identifier', default=IDENTIFIER, help='label of user identifier')
+    parser.add_argument('--statistics', action="store_true", help='use statistics mode')
+    parser.add_argument('--out', default='stories.svg', help='name of output svg file')
     args = parser.parse_args()
+    IDENTIFIER = args.identifier
     main(args)
